769f5b49df0b499b92e6cc5f628f5263802b9f81
Nils Brinkmann <monsdar+github@gmail.com>
2014-04-05 19:58:40 +0200

    Added: Primitive system to maintain an overview of which Activity started which. Works good with single-threaded applications as far as I can test for now

--- src/brofiler_dyn/NetworkBrofiler.h
+++ src/brofiler_dyn/NetworkBrofiler.h
@@ -2,6 +2,8 @@
 #ifndef _NETWORK_BROFILER_H_
 #define _NETWORK_BROFILER_H_
 
+#include <stack>
+
 #include "brofiler_dyn/common.h"
 #include "brofiler/IDynBrofiler.h"
 
@@ -20,6 +22,7 @@ private:
     void addResult(const ActivityResult& result);
     
     unsigned int actCounter_;
+	std::stack<unsigned int> activeActivity_;
 };
 
 #endif //_NETWORK_BROFILER_H_
--- src/brofiler_dyn/src/NetworkBrofiler.cpp
+++ src/brofiler_dyn/src/NetworkBrofiler.cpp
@@ -7,28 +7,37 @@
 
 NetworkBrofiler::NetworkBrofiler() :
     actCounter_(1)
-{}
+{
+	//zero means that there are no activities yet
+	activeActivity_.push(0);
+}
 
 NetworkBrofiler::~NetworkBrofiler()
 {}
 
 boost::shared_ptr<IActivity> NetworkBrofiler::createActivity(const std::string& name)
 {
+	unsigned int newActId = actCounter_++;
+
     boost::shared_ptr<NetworkActivity> newAct( new NetworkActivity() );
     newAct->setName(name);
-    newAct->setActId(actCounter_++);
-    newAct->setParentId(0);
+	newAct->setActId(newActId);
+	newAct->setParentId(activeActivity_.top());
     newAct->setThreadId(0);
     newAct->setResultCallback( boost::bind( &NetworkBrofiler::addResult, this, _1 ) );
     
     newAct->start();
+	activeActivity_.push(newActId);
     
     return newAct;
 }
 
 void NetworkBrofiler::addResult(const ActivityResult& result)
 {
-    std::cout << "Activity " << result.Name << " ended..." << std::endl;
+    std::cout << "Activity " << result.Name << " ended. Parent: " << result.ParentId << "..." << std::endl;
+	activeActivity_.pop();
+	std::cout << "Current active Activity: " << activeActivity_.top() << std::endl;
+	
 }
 
 std::string NetworkBrofiler::toString() const
