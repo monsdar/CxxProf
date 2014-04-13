1cf2b32b59b667d6145ac6bcd204330f3158e941
Nils Brinkmann <monsdar+github@gmail.com>
2014-04-13 13:34:46 +0200

    Added: Some code to test if the integration works as expected

--- src/brofiler_server/data/startServer.bat
+++ src/brofiler_server/data/startServer.bat
@@ -9,5 +9,7 @@ copy %~dp0\..\..\brofiler_dyn_network\bin\brofiler_dyn_network.dll .
 set PATH=%PATH%;%~dp0/../../../thirdparty/boost/bin/
 set PATH=%PATH%;%~dp0/../../../thirdparty/pluma/bin/
 set PATH=%PATH%;%~dp0/../../../thirdparty/zmq/bin/
+set PATH=%PATH%;%~dp0/../../../thirdparty/sqlite3/bin/
+set PATH=%PATH%;%~dp0/../../../thirdparty/sqlite3cc/bin/
 
 %~dp0\brofiler_server.exe
--- src/brofiler_server/src/main.cpp
+++ src/brofiler_server/src/main.cpp
@@ -5,6 +5,7 @@
 #include <boost/thread/thread.hpp>
 #include <boost/shared_ptr.hpp>
 
+#include <sqlite3cc.h>
 #include <zhelpers.hpp>
 
 #include <iostream>
@@ -38,10 +39,37 @@ void receive()
     std::cout << "Received result for " << result.Name << std::endl;
 }
 
+void checkSqliteCode()
+{
+    sqlite::connection conn("data.s3db");
+    sqlite::query queryAllActivities(conn, "SELECT * FROM activities");
+    std::cout << "Results: " << queryAllActivities.num_results() << std::endl;
+
+    for (sqlite::query::iterator iter = queryAllActivities.begin(); iter != queryAllActivities.end(); iter++)
+    {
+        unsigned int actId = iter->column< unsigned int >(0);
+        unsigned int threadId = iter->column< unsigned int >(1);
+        unsigned int parentId = iter->column< unsigned int >(2);
+        unsigned int startTime = iter->column< unsigned int >(3);
+        unsigned int stopTime = iter->column< unsigned int >(4);
+        std::string name = iter->column< std::string >(5);
+
+        std::cout << std::endl;
+        std::cout << "Row Number: " << iter->row_number() << std::endl;
+        std::cout << "Activity " << name << " (" << actId << ")" << std::endl;
+        std::cout << "Thread: " << threadId << std::endl;
+        std::cout << "Parent: " << parentId << std::endl;
+        std::cout << "Start: " << startTime << std::endl;
+        std::cout << "Stop: " << stopTime << std::endl;
+    }
+}
+
 int main()
 {
     std::cout << "Starting Server" << std::endl;
     zmqListener_.bind("tcp://*:15232");
+
+    checkSqliteCode();
 
     std::cout << "Listening..." << std::endl;
     boost::thread listenThread(receive);
