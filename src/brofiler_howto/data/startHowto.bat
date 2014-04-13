42ab285025c87b82c0240e3788f1457af7109df0
Nils Brinkmann <monsdar+github@gmail.com>
2014-04-13 11:07:51 +0200

    Fixed: Now the start-scripts work as I want them to do

--- src/brofiler_howto/data/startHowto.bat
+++ src/brofiler_howto/data/startHowto.bat
@@ -3,13 +3,12 @@
 
 REM Copy to install/bin directory and execute
 
-DEL brofiler_dyn_network.dll
-copy ..\install\brofiler_dyn_network\bin\brofiler_dyn_network.dll .
-copy ..\install\brofiler_howto\bin\brofiler_howto.exe .
+DEL %~dp0\brofiler_dyn_network.dll
+copy %~dp0\..\..\brofiler_dyn_network\bin\brofiler_dyn_network.dll .
 
-set PATH=%PATH%;../thirdparty/boost/bin/
-set PATH=%PATH%;../thirdparty/pluma/bin/
-set PATH=%PATH%;../thirdparty/zmq/bin/
-set PATH=%PATH%;../install/brofiler_static/bin/
+set PATH=%PATH%;%~dp0/../../../thirdparty/boost/bin/
+set PATH=%PATH%;%~dp0/../../../thirdparty/pluma/bin/
+set PATH=%PATH%;%~dp0/../../../thirdparty/zmq/bin/
+set PATH=%PATH%;%~dp0/../../../install/brofiler_static/bin/
 
-brofiler_howto.exe
+%~dp0\brofiler_howto.exe
