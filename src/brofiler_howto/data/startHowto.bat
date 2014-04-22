1960bc294bede9a4a1bc69fcaf8dd11cf930a72b
Nils Brinkmann <monsdar+github@gmail.com>
2014-04-22 15:50:33 +0200

    Fixed: No need to add Boost to the path as we're linking it statically now

--- src/brofiler_howto/data/startHowto.bat
+++ src/brofiler_howto/data/startHowto.bat
@@ -6,7 +6,6 @@ REM Copy to install/bin directory and execute
 DEL %~dp0\brofiler_dyn_network.dll
 copy %~dp0\..\..\brofiler_dyn_network\bin\brofiler_dyn_network.dll .
 
-set PATH=%PATH%;%~dp0/../../../thirdparty/boost/bin/
 set PATH=%PATH%;%~dp0/../../../thirdparty/pluma/bin/
 set PATH=%PATH%;%~dp0/../../../thirdparty/zmq/bin/
 set PATH=%PATH%;%~dp0/../../../install/brofiler_static/bin/
