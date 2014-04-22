b95b2e50e62acfb70ced9d61176897f7349d7785
Nils Brinkmann <monsdar+github@gmail.com>
2014-04-22 22:20:15 +0200

    Fixed: It is now possible to create more than 1 activity in the same scope

--- src/brofiler_static/Brofiler.h
+++ src/brofiler_static/Brofiler.h
@@ -10,13 +10,17 @@
 #include <brofiler_static/IActivity.h>
 
 #define BROFILER_ACTIVITY(NAME) \
-    boost::shared_ptr<IActivity> newActivity = BrofilerStatic::getBrofiler()->createActivity(NAME);
+    boost::shared_ptr<IActivity> BROFILER_HELPER_COMBINE(newActivity, __LINE__) = BrofilerStatic::getBrofiler()->createActivity(NAME);
 
 #define BROFILER_MARK(NAME) \
     BrofilerStatic::getBrofiler()->addMark(NAME);
 
 #define BROFILER_PLOT(NAME, VALUE) \
     BrofilerStatic::getBrofiler()->addPlotValue(NAME, VALUE);
+
+// helper macros, see stackoverflow question #10379691
+#define BROFILER_HELPER_COMBINE1(X,Y) X##Y  
+#define BROFILER_HELPER_COMBINE(X,Y) BROFILER_HELPER_COMBINE1(X,Y)
 
 #else
 
