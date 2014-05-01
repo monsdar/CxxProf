33e8e281dfa5665f55bd7ea6d3fa35c8005c05af
Nils Brinkmann <monsdar+github@gmail.com>
2014-05-01 21:15:28 +0200

    Added: Now test.py has the ability to run native ctest too

--- script/test.py
+++ script/test.py
@@ -11,7 +11,15 @@ DEV_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../'
 BUILD_PATH = DEV_PATH + "/build/"
 INSTALL_PATH = DEV_PATH + "/install/"
 THIRDPARTY_PATH = DEV_PATH + "/thirdparty/"
-CTEST_EXE = DEV_PATH + "/thirdparty/cmake/ctest.exe"
+
+CTEST_EXE = DEV_PATH + "/thirdparty/cmake/ctest"
+#NOTE: append the .exe suffix if we're on Windows
+if(os.name == "nt" ):
+    CTEST_EXE += ".exe"
+    
+if("--native-ctest" in sys.argv):
+    CTEST_EXE = "ctest"
+    sys.argv.remove("--native-ctest")
 
 def main():
     #set the PATH to find Thirdparty
